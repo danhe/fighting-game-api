9995d77a8622f87c305f0bd7681a9654ce482444
Dan HE <dan_emilie@yahoo.com>
2019-06-04 20:40:45 +0200

    Character migration

---
+++ db/migrate/20190604090134_create_characters.rb
@@ -0,0 +1,15 @@
+class CreateCharacters < ActiveRecord::Migration[5.2]
+  def change
+    create_table :characters do |t|
+      t.string :name
+      t.integer :current_life, default: 100
+      t.integer :max_life, default: 100
+      t.integer :power, default: 4
+      t.integer :level, default: 1
+      t.integer :current_experience, default: 0
+      t.integer :max_experience, default: 100
+
+      t.timestamps null: false
+    end
+  end
+end
